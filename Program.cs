8855da5adb2164e199f361cad2f9396389fd951d
Khushali Kavathiya <khushalikavathiya@Solvatives-Air.lan>
2025-06-23 10:31:30 +0530

    add connection string and register for database connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using BookManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
